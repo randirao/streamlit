7f0c1543cf39fb66042061358cec8bc657773401
Han2 <randirao4245@gmail.com>
2024-07-16 12:34:47 +0900

    3

--- pages/Result.py
+++ pages/Result.py
@@ -8,6 +8,8 @@ with col1:
     st.title(' ')
 
 with col2:
+    if st.session_state.name == 'NULL':
+        st.switch_page("Main.py")
     st.write(st.session_state.name)
-
+    
 st.write("결과 : ",st.session_state.point)
